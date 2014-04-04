3db5c0ada56fa0f43c17678cafd8395bb7ffa3ca
Hugo Scurti <hugoscurti@gmail.com>
2014-04-03 22:31:07 -0300

    Ajout d'un test pour exercice1

---
+++ src/test/java/ca/uqam/inf2015/jsonapplication/exercices/exercice1Test.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.uqam.inf2015.jsonapplication.exercices;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ch891311
+ */
+public class exercice1Test {
+    
+    public exercice1Test() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testMain() throws Exception {
+    }
+    
+}
